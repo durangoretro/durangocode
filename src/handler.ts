2f83a847dd67a566baed738739d1859ae6ba6046
zerasul <zerasul@gmail.com>
2023-11-12 20:12:10 +0100

    adding commands and configuration

---
+++ src/handler.ts
@@ -0,0 +1,119 @@
+import { readFileSync } from "fs";
+import path = require("path");
+import { Terminal } from "vscode";
+
+/**
+ * EXECUTION MODE:
+ * * PEDITA: Run on Perdita Emulator.
+ * * NANOBOOT: Run Using NanoBoot.
+ */
+export enum EXEC_MODE{
+    /**Perdita Emulator */
+    PERDITA,
+    /**Run Using NanoBoot */
+    NANOBOOT
+};
+
+export enum SYSTEM{
+    WINDOWS="w",
+    LINUX="l",
+    DARWIN="d",
+    DOCKER="docker"
+};
+
+/**
+ * Commands Handler: Handle the execution of the commands. This is the base class.
+ */
+export abstract class  CommandHandler{
+
+    /**
+     * Extension Folder path
+     */
+    protected extensionPath:string;
+
+
+    /**
+     * Class Constructor
+     * @param extensionPath extension Folder Path 
+     */
+    constructor(extensionPath:string){
+        this.extensionPath=extensionPath;
+    }
+    /**
+     * Handle the Compile Project Command
+     */
+    abstract compile():Boolean;
+    /**
+     * Handle the Clean Project Command.
+     */
+    abstract clean():Boolean;
+    /**
+     * Handle the Run Commands Handle
+     * @param mode Run Execution Mode. @see EXEC_MODE
+     */
+    abstract run(mode: EXEC_MODE):Boolean;
+    /**
+     * Handle the Create Project Command
+     * @param projectPath Project Root Path
+     */
+    abstract create(projectPath:String):Boolean;
+    /**
+     * Send a file or program using VSP (Virtual Serial Port)
+     * @param filePath File to Send
+     */
+    abstract sendVSP(filePath:String):Boolean;
+    /**
+     * Load information from VSP (Virtual Serial Port) and create a new File.
+     * @param filePath Path of the new file to be created. If exists, will be overwritten.
+     */
+    abstract loadVSP(filePath:String):Boolean;
+
+    /**
+     * Handle a new command searching in the commands.json file.
+     * 
+     * @param commandName command Name.
+     * @param system System used. @see SYSTEM
+     * @returns 
+     */
+    protected handleCommand(commandName:string,system:SYSTEM):string|undefined{
+
+        
+        let bufferJson = readFileSync(path.join(this.extensionPath,"resources","commands.json"));
+        let json= JSON.parse(bufferJson.toString());
+        try {
+            return json.commands[commandName][system];
+        } catch (error) {
+            return undefined;
+        }
+
+    }
+ };
+
+export class CommandWHandler extends CommandHandler{
+
+    public constructor(extensionPath: string){
+        super(extensionPath);
+    }
+
+    compile(): Boolean {
+       let command = this.handleCommand("compile",SYSTEM.WINDOWS);
+       //TODO: Execute command.
+       return command!==undefined;
+    }
+    clean(): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    run(mode: EXEC_MODE): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    create(projectPath: String): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    sendVSP(filePath: String): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    loadVSP(filePath: String): Boolean {
+        throw new Error("Method not implemented.");
+    }
+
+}
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,6 +3,8 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import * as handler from '../../handler';
+import path = require('path');
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
@@ -11,5 +13,9 @@ suite('Extension Test Suite', () => {
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
+	test('Handler test',()=>{
+		let handl= new handler.CommandWHandler(path.join(__dirname,"../../.."));
+		assert.strictEqual(handl.compile(),true);
 	});
 });
