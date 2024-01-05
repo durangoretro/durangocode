75cad601c7d8c42d686d7f3cb07b05df788ed593
vsuarez <zerasul@gmail.com>
2024-01-05 13:50:36 +0100

    WIP: adding commands implementations

---
+++ src/DurangoConstants.ts
@@ -0,0 +1,23 @@
+//Configuration
+
+export const DDK = "DDK";
+
+export const CUSTOMRESCOMP = "CustomRescompJar";
+
+export const EXECUTIONMODECONFIG= "ExecutionMode";
+
+export const PERDITAPATHCONFIG= "PerditaPath";
+
+export const NANOBOOTPATHCONFIG="NanobootPath";
+
+//Commands
+
+export const COMPILE = "compile";
+
+export const CLEAN = "clean";
+
+export const RUN = "run";
+
+
+//Others
+export const DURANGOCODE = 'Durango.code';
--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from "fs";
 import path = require("path");
 import * as vscode from "vscode";
+import * as DurangoConstants from "./DurangoConstants";
 
 /**
  * EXECUTION MODE:
@@ -14,11 +15,15 @@ export enum EXEC_MODE{
     NANOBOOT
 };
 
+/**
+ * Current System Configuration
+ */
 export enum SYSTEM{
     WINDOWS="w",
     LINUX="l",
     DARWIN="d",
-    DOCKER="docker"
+    DOCKER="docker",
+    UNKNOWN="UNKNOWN"
 };
 
 /**
@@ -31,10 +36,12 @@ export abstract class  CommandHandler{
      */
     protected extensionPath:string;
 
+    protected System:SYSTEM;
+
     /**
      * Durango Code Integrated Terminal
      */
-    protected terminal:vscode.Terminal;
+    protected terminal:vscode.Terminal|undefined;
 
 
     /**
@@ -43,21 +50,20 @@ export abstract class  CommandHandler{
      */
     constructor(extensionPath:string){
         this.extensionPath=extensionPath;
-        this.terminal= vscode.window.createTerminal('Durango.code');
+        this.terminal= undefined;
+        this.System=SYSTEM.UNKNOWN;
     }
+
     /**
-     * Handle the Compile Project Command
+     * Updates the current environmentVariables using current Configuration
      */
-    abstract compile():Boolean;
-    /**
-     * Handle the Clean Project Command.
-     */
-    abstract clean():Boolean;
+    abstract setEnvironmentVariables():void;
+  
     /**
      * Handle the Run Commands Handle
      * @param mode Run Execution Mode. @see EXEC_MODE
      */
-    abstract run(mode: EXEC_MODE):Boolean;
+    abstract run(mode: EXEC_MODE, newLine:boolean):Boolean;
     /**
      * Handle the Create Project Command
      * @param projectPath Project Root Path
@@ -79,7 +85,7 @@ export abstract class  CommandHandler{
      * 
      * @param commandName command Name.
      * @param system System used. @see SYSTEM
-     * @returns 
+     * @returns True if the command was successfully handled
      */
     protected handleCommand(commandName:string,system:SYSTEM):string|undefined{
 
@@ -94,25 +100,146 @@ export abstract class  CommandHandler{
 
     }
 
-    protected executeCommand(command:string,newLine:boolean=true){
-        this.terminal.sendText(command,newLine);
+    /**
+     * Gets the current Durango.code Terminal, or create a new One.
+     * @returns Current DurangoCode Terminal
+     */
+    protected getCurrentTerminal():vscode.Terminal{
+        if(this.terminal==undefined){
+            let terminals = vscode.window.terminals.filter(terminal => terminal.name === DurangoConstants.DURANGOCODE);
+            this.terminal= terminals.length>0?terminals[0]:vscode.window.createTerminal(DurangoConstants.DURANGOCODE);
+        }
+        this.terminal.show(true);
+        return this.terminal;
     }
+
+    /**
+     * Execute current command on Terminal
+     * @param command Command to be executed
+     * @param newLine Adds a new Line character at the end or not (for compose commands).
+     */
+    protected executeCommand(command:string,newLine:boolean=true){
+        this.getCurrentTerminal().sendText(command,newLine);
+    }
+    /**
+     * Handle the Compile Project Command
+     */
+    public compile(newLine:Boolean=true): Boolean {
+        let command:string|undefined = this.handleCommand(DurangoConstants.COMPILE,this.System);
+        //TODO: Execute command.
+        this.setEnvironmentVariables();
+        if(command!==undefined)
+                 this.executeCommand(command,true);
+        return command!==undefined;
+     }
+
+    /**
+     * Current Execution Mode From Configuration
+     * @returns Get Current Execution Mode From Configuration
+     */
+    protected getCurrentExecMode():EXEC_MODE{
+        let execConfig = vscode.workspace.getConfiguration().get(DurangoConstants.EXECUTIONMODECONFIG,EXEC_MODE.PERDITA.toString());
+
+        let execKey= execConfig as keyof typeof EXEC_MODE;
+
+        return EXEC_MODE[execKey];
+    }
+
+    /**
+     * Handle the Clean Project Command.
+     */
+    public clean(): Boolean {
+        this.setEnvironmentVariables();
+        let command=this.handleCommand(DurangoConstants.CLEAN,this.System);
+        if(command){
+         this.executeCommand(command);
+        }
+        return command!==undefined;
+     }
+
+     public compileAndRun():Boolean{
+       return this.compile(false) && this.run(this.getCurrentExecMode(),false);
+     }
  };
 
 export class CommandWHandler extends CommandHandler{
+  
 
     public constructor(extensionPath: string){
         super(extensionPath);
+        this.System=SYSTEM.WINDOWS;
     }
 
-    compile(): Boolean {
-       let command:string|undefined = this.handleCommand("compile",SYSTEM.WINDOWS);
-       //TODO: Execute command.
-       
-       if(command!==undefined)
-                this.executeCommand(command);
-       return command!==undefined;
+    
+    setEnvironmentVariables(): void {
+        //Set DDK Variable
+        let DDKVariable=vscode.workspace.getConfiguration().get(DurangoConstants.DDK);
+        if(DDKVariable){
+           this.executeCommand(`set DDK="${DDKVariable}"`);
+        }
+        //Set custom RescompPath
+        let rescompPath = vscode.workspace.getConfiguration().get(DurangoConstants.CUSTOMRESCOMP);
+        if(rescompPath){
+            this.executeCommand(`set DDK="${rescompPath}"`);
+         }
     }
+
+   
+
+    run(mode: EXEC_MODE, newLine:Boolean=true): Boolean {
+        if(!newLine){
+            this.executeCommand( ' && ', false);
+        }
+        let executable="";
+        switch(mode){
+            case EXEC_MODE.PERDITA:
+                executable=vscode.workspace.getConfiguration().get(DurangoConstants.PERDITAPATHCONFIG,"perdita");
+            break;
+            case EXEC_MODE.NANOBOOT:
+                executable=vscode.workspace.getConfiguration().get(DurangoConstants.NANOBOOTPATHCONFIG,"nanoboot");
+            break;
+        }
+        let executeCommand:string|undefined = this.handleCommand(DurangoConstants.RUN,this.System);
+
+        executeCommand?.replace("${executable}",executable);
+        //TODO: Review better implementation
+        executeCommand?.replace("${romFile}", "build/rom.bin");
+        if(executeCommand)
+            this.executeCommand(executeCommand);
+        return executeCommand!==undefined;
+    }
+    
+    create(projectPath: String): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    sendVSP(filePath: String): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    loadVSP(filePath: String): Boolean {
+        throw new Error("Method not implemented.");
+    }
+
+};
+
+export class CommandLHandler extends CommandHandler{
+
+    public constructor(extensionPath:string){
+        super(extensionPath);
+        this.System=SYSTEM.LINUX;
+    }
+
+    setEnvironmentVariables(): void {
+         //Set DDK Variable
+        let DDKVariable=vscode.workspace.getConfiguration().get(DurangoConstants.DDK);
+        if(DDKVariable){
+           this.executeCommand(`export DDK="${DDKVariable}"`);
+        }
+        let rescompPath = vscode.workspace.getConfiguration().get(DurangoConstants.CUSTOMRESCOMP);
+        if(rescompPath){
+            this.executeCommand(`export DDK="${rescompPath}"`);
+         }
+    }
+
     clean(): Boolean {
         throw new Error("Method not implemented.");
     }
