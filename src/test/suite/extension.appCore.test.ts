2eb2fe3a86939702ae8dafd89b509454143e00ec
vsuarez <zerasul@gmail.com>
2024-01-10 21:50:31 +0100

    adding appCore test Clean

--- src/test/suite/extension.appCore.test.ts
+++ src/test/suite/extension.appCore.test.ts
@@ -1,26 +1,34 @@
 import * as assert from 'assert';
-
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as handler from '../../handler';
 import path = require('path');
-import * as fs from 'fs';
-import {tmpdir} from "os";
-import { CommandData } from '../../utils';
 import { before } from 'mocha';
 import { AppCore } from '../../appCore';
-import * as vscodeTest from '@vscode/test-electron';
+import * as DurangoConstants from '../../DurangoConstants';
+import * as vscode from 'vscode';
 
-let appCore:AppCore;
+let appCore: AppCore;
 
-suite('AppCore Test',()=>{
-    before(()=>{
-        appCore=new AppCore(path.join(__dirname,"../../.."));
-        
+let getTerminal=()=>{
+    let terminals = vscode.window.terminals.filter(terminal => terminal.name === DurangoConstants.DURANGOCODE);
+    return terminals[0];
+}
+
+suite('AppCore Test', () => {
+    before(() => {
+        appCore = new AppCore(path.join(__dirname, "../../.."));
+
     });
-    test('Test Compile', () =>{
+    test('Test Compile', () => {
         appCore.compile();
+        let terminal = getTerminal();
+        assert.strictEqual(terminal.name, DurangoConstants.DURANGOCODE);
+        terminal.hide();
     });
-
+    test('Test Clean', () => {
+        appCore.clean();
+        let terminal = getTerminal();
+        assert.strictEqual(terminal.name, DurangoConstants.DURANGOCODE);
+        terminal.hide();
+    });
 });
